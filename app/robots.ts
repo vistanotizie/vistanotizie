4b92bc7beb5fdbe4a465a37a3c03276a032c2b00
vistanotizie <vistanotizie.project@gmail.com>
2026-04-05 12:27:04 +0200

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,8 +1,16 @@
 import type { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
+  const siteUrl = 'https://vistanotizie.it';
+
   return {
-    rules: [{ userAgent: '*', allow: '/' }],
-    sitemap: 'https://example.com/sitemap.xml',
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    sitemap: `${siteUrl}/sitemap.xml`,
+    host: siteUrl,
   };
 }
