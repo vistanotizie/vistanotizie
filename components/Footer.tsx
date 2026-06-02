32ea0262fa4c7dd918eaaf907e6a1d63fa84afce
vistanotizie <vistanotizie.project@gmail.com>
2026-06-02 16:19:45 +0200

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+export function Footer() {
+  return (
+    <footer className="site-footer">
+      <div>
+        <strong>VistaNotizie</strong>
+        <p>
+          Le ultime notizie dall'Italia e dal mondo, aggiornate durante la giornata.
+        </p>
+      </div>
+
+      <div>
+        © 2026 VistaNotizie
+      </div>
+    </footer>
+  );
+}
